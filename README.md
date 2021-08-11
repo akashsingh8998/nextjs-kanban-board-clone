05739f28fd0eca8fae5ced83a5884170c22489b9
Akash Pradeep Singh <akash.singh@publicissapient.com>
2021-08-11 10:50:51 +0530

    added readme and modified mock data

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-This is a starter template for [Learn Next.js](https://nextjs.org/learn).
+Instructions to run.
+1. Clone the git repo
+2. run `npm i`
+3. run `npm run dev`
--- utils/mockData/kanbanBoardData.js
+++ utils/mockData/kanbanBoardData.js
@@ -5,33 +5,21 @@ const data = {
       cards: {
         'card-11': {
           id: 'card-11',
-          title: 'Learning how to cook1',
+          title: 'Learn how to cook!',
         },
         'card-12': {
           id: 'card-12',
-          title: 'Making sandwich1',
-        },
-        'card-13': {
-          id: 'card-13',
-          title: 'Taking the trash out1',
-        },
+          title: 'Read a book!',
+        }
       },
     },
     'list-2': {
       id: 'list-2',
       title: 'Doing',
       cards: {
-        'card-21': {
-          id: 'card-21',
-          title: 'Learning how to cook2',
-        },
         'card-22': {
           id: 'card-22',
-          title: 'Making sandwich2',
-        },
-        'card-23': {
-          id: 'card-23',
-          title: 'Taking the trash out2',
+          title: 'Studying for exams!',
         },
       },
     },
@@ -39,17 +27,9 @@ const data = {
       id: 'list-3',
       title: 'Done',
       cards: {
-        'card-31': {
-          id: 'card-31',
-          title: 'Learning how to cook3',
-        },
-        'card-32': {
-          id: 'card-32',
-          title: 'Making sandwich3',
-        },
         'card-33': {
           id: 'card-33',
-          title: 'Taking the trash out3',
+          title: 'Taking the trash out',
         },
       },
     }
